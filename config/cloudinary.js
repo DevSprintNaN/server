4c9527843d938c4d13b319e508acb8b49bf57a72
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-02-13 01:10:33 +0600

    cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,9 +6,28 @@ cloudinary.config({
   api_secret: process.env. _CLOUDINARY_API_SECRET
 });
 
-const uploadFile = async (file)=>{
-  await cloudinary.uploader.upload(`https://upload.wikimedia.org/wikipedia/commons/a/ae/${file}`,
-  function(error, result) {console.log(result); });
+const uploadFile = async (user, file)=>{
+
+  try{
+
+    const currentDate = new Date();
+    const options = { timeZone: 'Asia/Dhaka' }; // Set the timezone to GMT+6 (Asia/Dhaka)
+    const formattedDate = currentDate.toLocaleString('en-US', options)  
+      .replace(/:/g, "-") // Replace colons with hyphens
+      .replace(/\//g, "-"); // Replace slashes with hyphens
+
+    const b64 = Buffer.from(file.buffer).toString("base64");
+    let dataURI = "data:" + file.mimetype + ";base64," + b64;
+    const result = await cloudinary.uploader.upload(dataURI,{
+      resource_type:"auto",
+      public_id: `${file.originalname}$_${user._id}$_${formattedDate}`
+    });
+    return result;
+  }catch(error){
+    console.log(error);
+    return error;
+  }
+  
 };
 
 module.exports = uploadFile;
