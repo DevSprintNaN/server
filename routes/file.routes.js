0998e899ab9095890499a1a75f7e500e8f87e78b
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-02-15 14:51:53 +0600

    restore previous version

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -1,4 +1,9 @@
-const {uploader,fetchFiles,getFileChanges} = require('../controller/file.controller');
+const {
+    uploader,
+    fetchFiles,
+    getFileChanges,
+    restorePreviousVersion
+} = require('../controller/file.controller');
 const upload = require('../middleware/multer.middleware');
 const router = require('express').Router();
 const authenticated = require('../middleware/auth.middleware');
@@ -7,4 +12,5 @@ const authenticated = require('../middleware/auth.middleware');
 router.post('/upload', authenticated, upload.single('file'), uploader);
 router.get('/get-all/:id', authenticated, fetchFiles);
 router.get('/get-changes/:file_name', authenticated, getFileChanges);
+router.put('/restore', authenticated, restorePreviousVersion);
 module.exports = router;
