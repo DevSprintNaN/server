e5e385eb5e43983184075d50c57053d47351db11
Hasnain Kabir <hasnainkabir@iut-dhaka.edu>
2024-02-16 00:15:05 +0600

    contributors

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -2,15 +2,18 @@ const {
     uploader,
     fetchFiles,
     getFileChanges,
-    restorePreviousVersion
+    restorePreviousVersion,
 } = require('../controller/file.controller');
 const upload = require('../middleware/multer.middleware');
 const router = require('express').Router();
 const authenticated = require('../middleware/auth.middleware');
 
+const {getContributorById} = require('../controller/contribution.controller');
 
 router.post('/upload', authenticated, upload.single('file'), uploader);
 router.get('/get-all/:id', authenticated, fetchFiles);
 router.get('/get-changes/:file_name', authenticated, getFileChanges);
 router.put('/restore', authenticated, restorePreviousVersion);
+
+router.get('/get-contributor/:id', authenticated, getContributorById)
 module.exports = router;
