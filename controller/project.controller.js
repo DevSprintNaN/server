528bdbea10278f3ccf6bfe9bfeabdedb86a7d6c7
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-19 00:38:57 +0600

    Popular Projects Front end Integration

--- controller/project.controller.js
+++ controller/project.controller.js
@@ -41,7 +41,7 @@ const getProjects=async(req,res)=>{
 
 const getAllProjects=async(req,res)=>{
     try{
-        const projects = await Project.find({});
+        const projects = await Project.find({}).sort({stars:-1}).limit(100);
         res.status(200).json({status:"success", projects});
     }
     catch(error){
@@ -50,5 +50,103 @@ const getAllProjects=async(req,res)=>{
     }
 }
 
+const getProject=async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const project=await Project.findById(id);
+        if(!project){
+            return res.status(400).json({status:"error", message:"Project not found"});
+        }
+        res.status(200).json({status:"success", project});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:error});
+    }
+}
 
-module.exports = {createProject,getProjects,getAllProjects};
+
+const starProject=async(req,res)=>{
+    try{
+        const {id} = req.body;
+        const project=await Project.findById(id);
+        if(!project){
+            return res.status(400).json({status:"error", message:"Project not found"});
+        }
+        project.stars=project.stars+1;
+        await project.save();
+        const user=await User.findById(req.user._id);
+        user.starred_projects.push(id);
+        await user.save();
+        res.status(200).json({status:"success", message:"Project starred successfully"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:error});
+    }
+}
+
+const unstarProject=async(req,res)=>{
+    try{
+        const {id} = req.body;
+        const project=await Project.findById(id);
+        if(!project){
+            return res.status(400).json({status:"error", message:"Project not found"});
+        }
+        project.stars=project.stars-1;
+        await project.save();
+        const user=await User.findById(req.user._id);
+        user.starred_projects=user.starred_projects.filter(project=>project!==id);
+        await user.save();
+        res.status(200).json({status:"success", message:"Project unstarred successfully"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:error});
+    }
+}
+
+const isProjectStarred=async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const user=await User.findById(req.user._id);
+        const isStarred=user.starred_projects.includes(id);
+        res.status(200).json({status:"success", isStarred});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:error});
+    }
+}
+
+const numberOfContributorsPerProject=async(req,res)=>{
+    try{
+        const projects=await Project.find({}).sort({stars:-1}).limit(100);
+        const contributors=[];
+        projects.forEach((project)=>{
+            contributors.push(project.users.length);
+        })
+        return res.status(200).json({status:"success", contributors});
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error});
+    }
+}
+
+const numberOfStarsPerProject=async(req,res)=>{
+    try{
+        const projects=await Project.find({}).sort({stars:-1}).limit(100);
+        const stars=[];
+        projects.forEach((project)=>{
+            stars.push(project.stars);
+        })
+        return res.status(200).json({status:"success", stars});
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error});
+    }
+}
+
+module.exports = {createProject,getProjects,getAllProjects,starProject,unstarProject,getProject,isProjectStarred,numberOfContributorsPerProject,numberOfStarsPerProject};
