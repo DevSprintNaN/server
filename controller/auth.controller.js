1e424cc8089cdfa318950cf57ca5ce87ce9f9192
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-18 17:43:13 +0600

    Shifting to JWT

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
-const passport = require('passport');
-var CryptoJS = require("crypto-js");
+const generateJwt = require('../config/jwt_generator');
+const passport=require('../config/passport')(require('passport'));
 require('dotenv').config();
 
 const checkExistingUser = async(user) => {
@@ -32,7 +32,8 @@ const register = async(req, res) => {
                 password: hash
             }
             const createdUser=await User.create(newUser);
-            res.status(200).json({message: "User created successfully",token: CryptoJS.HmacSHA512(createdUser._id, process.env._TOKEN_SECRET).toString()});
+            const token=await generateJwt(createdUser);
+            res.status(200).json({message: "User created successfully", token, user: createdUser});
         }
     }catch(error){
         console.log(error);
@@ -40,59 +41,32 @@ const register = async(req, res) => {
     }
 };
 
-const login = async (req, res, next) => {
-    passport.authenticate("local", async (err, user, info) => {
-        if (err) {
-            return res.status(500).json({ status: "error", message: err.message });
-        }
-        if (!user) {
-            return res.status(401).json({ status: "error", message: info.message });
-        }
-        
-        req.login(user, (err) => {
-            if (err) {
-                return res.status(500).json({ status: "error", message: err.message });
-            }
-            return res.status(200).json({ status: "success", message: "Login successful", user: user,token: CryptoJS.HmacSHA512(user._id, process.env._TOKEN_SECRET).toString()});
-        });
-    })(req, res, next);
+const login = async (req, res) => {
+    try{
+        const {email,password}=req.body;
+        const user = await User.login(email,password);
+        res.status(200).json({status:"success", message:"Logged in successfully",...user});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({error:error});
+    }
 };
 
-const validateUser = async(req,res)=>{
-    if(req.isAuthenticated()){
-        res.status(200).json({status:"success", message:"User is authenticated",token:CryptoJS.HmacSHA512(req.user._id, process.env._TOKEN_SECRET).toString()});
-    }
-    else{
-        res.status(401).json({status:"error", message:"User is not authenticated"});
-    }
-}
-
-const getUser=async(req,res)=>{
-    if(req.isAuthenticated()){
-        res.status(200).json({status:"success", message:"User is authenticated",user:{_id:req.user._id,email:req.user.email,username:req.user.username}});
-    }
-    else{
-        res.status(401).json({status:"error", message:"User is not authenticated"});
-    }
-}
-
-
-
-const logout = async(req, res) =>{
-    req.logout(function(err) {
-        if (err) { 
-            console.log(err);
-            res.status(500).json({status:"error", error:err});
-        }else{
-            res.status(200).json({status:"success", message:"Logged out successfully"});
+const validateUser=async(req,res)=>{
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+        if (err || !user) {
+          return res.status(401).json({ message: 'Unauthorized' });
         }
-      });
-};
+        else{
+            return res.status(200).json({status:"success", message:"User is authenticated"});
+        }
+      })(req, res);
+}
+
 
 module.exports = {
     register,
     login,
-    logout,
-    validateUser,
-    getUser
+    validateUser
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,15 +2,11 @@ const router = require('express').Router()
 const {
     register,
     login,
-    logout,
-    validateUser,
-    getUser
+    validateUser
 } = require('../controller/auth.controller');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
 router.post('/verify', validateUser);
-router.post('/get-user',getUser);
 
 module.exports = router;
