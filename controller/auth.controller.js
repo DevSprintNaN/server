3292e06fdebf71ff66d89145da1cf080d65daaf3
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-19 17:49:10 +0600

    Email Verification+Forgot Password Backend

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,8 +1,10 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
 const generateJwt = require('../config/jwt_generator');
+const Otp = require('../models/Otp.model');
 const passport=require('../config/passport')(require('passport'));
 require('dotenv').config();
+const SMTPClient = require('../config/smtp');
 
 const checkExistingUser = async(user) => {
     try{
@@ -82,10 +84,102 @@ const getUser=async(req,res)=>{
       })(req, res);
 }
 
+const verifyEmail=async(req,res)=>{
+    try{
+        const {email}=req.body;
+        const user=await User.findOne({email});
+        if(user){
+            const mailer=new SMTPClient(email);
+            const otp=mailer.generateOTP();
+            await Otp.create({email,otp});
+            mailer.generateMail();
+            mailer.sendVerificationMail();
+            return res.status(200).json({status:"success"})
+        }
+        else{
+            return res.status(400).json({status:"error",message:"User does not exist"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error,status:"error"})
+    }
+}
+
+const verifyOtp=async(req,res)=>{
+    try{
+        const {email,otp}=req.body;
+        const otpDoc=await Otp.findOne({email,otp});
+        if(otpDoc){
+            const user=await User.findOne({email});
+            user.verified=true;
+            await user.save();
+            await Otp.findOneAndDelete({email,otp});
+            return res.status(200).json({status:"success",message:"User verified"});
+        }
+        else{
+            return res.status(400).json({status:"error",message:"Invalid otp"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error,status:"error",message:"Internal Server Error",status:"error"})
+    }
+}
+
+const otpForgotPassword=async(req,res)=>{
+    try{
+        const {email}=req.body;
+        const user=await User.findOne({email});
+        if(user){
+            const mailer=new SMTPClient(email);
+            const otp=mailer.generateOTP();
+            await Otp.create({email,otp});
+            mailer.generateMail();
+            mailer.sendVerificationMail();
+            return res.status(200).json({status:"success"});
+        }
+        else{
+            return res.status(400).json({status:"error",message:"User does not exist"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error,status:"error"})
+    }
+}
+
+const resetPassword=async(req,res)=>{
+    try{
+        const {email,otp,password}=req.body;
+        const otpDoc=await Otp.findOne({email,otp});
+        if(otpDoc){
+            const user=await User.findOne({email});
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+            user.password=hash;
+            await user.save();
+            await Otp.findOneAndDelete({email,otp});
+            return res.status(200).json({status:"success",message:"Password reset successfully"});
+        }
+        else{
+            return res.status(400).json({status:"error",message:"Invalid otp"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({error:error,status:"error",message:"Internal Server Error"})
+    }
+}
+
 
 module.exports = {
     register,
     login,
     validateUser,
-    getUser
+    getUser,
+    verifyEmail,
+    verifyOtp,
+    otpForgotPassword,
+    resetPassword
 }
